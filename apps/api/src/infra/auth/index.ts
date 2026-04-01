5dcffd081168e7db6e4db1676722d869c4bb12cf
kevinclarkadstech <hellokevinclark@gmail.com>
2026-04-01 16:40:44 -0700

    follow similar pattern

---
+++ apps/api/src/infra/auth/index.ts
@@ -0,0 +1,1 @@
+export { fastifyAuthHandler } from "./fastify-auth-handler";
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -36,7 +36,6 @@ import "@ionic/react/css/palettes/dark.system.css";
 /* Theme variables */
 import "./theme/variables.css";
 import { queryClient } from "./dependencies";
-import { AppRouter } from "@myapp/api/src/trpc";
 
 setupIonicReact();
 
