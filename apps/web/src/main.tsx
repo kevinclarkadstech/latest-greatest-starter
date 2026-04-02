892105e0d68d09197d915f8f40514129011d77f0
kevinclarkadstech <hellokevinclark@gmail.com>
2026-04-02 16:40:33 -0700

    Fix up cache a bit

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById('root');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { createInMemoryCache } from "./dependencies/cache/in-memory-cache";
+export const testCache = createInMemoryCache();
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
