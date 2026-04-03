8726b2e84158ef6d015f84cd3816a4acac02ef97
kevinclarkadstech <hellokevinclark@gmail.com>
2026-04-03 14:07:36 -0700

    initial commit

--- apps/api/src/infra/auth/better-auth.ts
+++ apps/api/src/infra/auth/better-auth.ts
@@ -1,33 +1,12 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { NeonHttpDatabase } from "drizzle-orm/neon-http";
-import { NeonQueryFunction } from "@neondatabase/serverless";
-import { NodePgDatabase } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { AppDb } from "../db"; // Use the type we created earlier
+import { authConfig } from "./better-auth.config";
 
-export function createBetterAuthClient({
-  db,
-}: {
-  db:
-    | (NeonHttpDatabase<any> & {
-        $client: NeonQueryFunction<false, false>;
-      })
-    | (NodePgDatabase<any> & {
-        $client: Pool;
-      });
-}) {
-  const betterAuthClient = betterAuth({
-    // database: new Pool({
-    //   connectionString: process.env.DATABASE_URL,
-    // }),
-    database: drizzleAdapter(db, {
-      provider: "pg", // or "pg" or "mysql"
-    }),
-    emailAndPassword: {
-      enabled: true,
-    },
-    baseURL: process.env.BASE_URL!,
+export function createBetterAuthClient({ db }: { db: AppDb }) {
+  return betterAuth({
+    ...authConfig,
+    // We override the database function here with the actual instance
+    database: drizzleAdapter(db, { provider: "pg" }),
   });
-
-  return betterAuthClient;
 }
